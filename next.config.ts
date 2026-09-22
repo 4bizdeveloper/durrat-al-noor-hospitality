6c461b72c7e3260a1dbff551fe1fcc5608fe2730
4bizdeveloper <4bizdeveloper@gmail.com>
2026-09-22 14:56:36 +0530

    Build responsive Next.js foundation

---
+++ next.config.ts
@@ -0,0 +1,1 @@
+import type {NextConfig} from "next"; const nextConfig:NextConfig={images:{remotePatterns:[{protocol:"https",hostname:"images.unsplash.com"}]},poweredByHeader:false,compress:true}; export default nextConfig;
